b8f8c6e4224b657af74a8a916dfc46d5fdc47ffb
aytea14 <muhdamar4687@gmail.com>
2024-08-07 22:39:31 +0800

    Update Renderer.java

--- src/client/java/dev/ujol/hud/renderer/Renderer.java
+++ src/client/java/dev/ujol/hud/renderer/Renderer.java
@@ -1,14 +1,13 @@
 package dev.ujol.hud.renderer;
 
-import java.awt.Color;
-
 import com.mojang.blaze3d.systems.RenderSystem;
-
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.text.Text;
+
+import java.awt.*;
 
 public class Renderer {
     protected static final MinecraftClient client = MinecraftClient.getInstance();
@@ -16,6 +15,14 @@ public class Renderer {
     protected void renderText(DrawContext drawContext, Text text, int x, int y, Color color) {
         TextRenderer textRenderer = client.textRenderer;
         drawContext.drawText(textRenderer, text, x, y, color.getRGB(), false);
+    }
+
+    protected void renderMultiLineText(DrawContext drawContext, Text[] texts, int x, int y, Color color) {
+        TextRenderer textRenderer = client.textRenderer;
+        for (int i = 0; i < texts.length; i++) {
+            Text text = texts[i];
+            drawContext.drawText(textRenderer, text, x, y + (i * 13), color.getRGB(), false);
+        }
     }
 
     protected void renderBackground(DrawContext drawContext, int x, int y, int width, int height, Color color) {
