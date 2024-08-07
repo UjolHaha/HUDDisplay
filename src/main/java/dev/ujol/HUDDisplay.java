b6112459db256fe4ec15690b229beec36a956684
aytea14 <muhdamar4687@gmail.com>
2024-08-08 01:56:22 +0800

    update

---
+++ src/main/java/dev/ujol/HUDDisplay.java
@@ -0,0 +1,16 @@
+package dev.ujol;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HUDDisplay implements ModInitializer {
+	public static final String MOD_ID = "huddisplay";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
